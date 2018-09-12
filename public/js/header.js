f1a6bedd14eb34bbcb427e7dcd725a486b02c593
Kh3m <khem6333@gmail.com>
2018-09-12 23:38:55 +0100

    Correct i rotation

--- public/js/header.js
+++ public/js/header.js
@@ -1,5 +1,5 @@
 const liNav = document.querySelector("#bottom-header nav ul");
-
+    
 let clickCount = 1;
 
 liNav.addEventListener("click", ( event ) => {
@@ -10,10 +10,13 @@ liNav.addEventListener("click", ( event ) => {
         event.target.nextSibling.nextSibling.style.transform = "translateY(0)";
         event.target.nextSibling.nextSibling.style.opacity = "1";
         event.target.nextSibling.nextSibling.style.height = "auto";
+        event.target.children[0].style.transform = "rotate(180deg)";
     } else {
         event.target.nextSibling.nextSibling.style.transform = "translateY(5px)";
         event.target.nextSibling.nextSibling.style.opacity = "0";
         event.target.nextSibling.nextSibling.style.height = "0";
+        event.target.children[0].style.transform = "rotate(0deg)";
+
     }
 
 });
